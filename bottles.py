0bb68fc4f6dbef1e4b6dedd138ad799c88458d4e
Hank Caswell <david.h.caswell@proton.me>
2024-02-01 14:57:35 -0500

    finished initial solution

--- bottles.py
+++ bottles.py
@@ -1,16 +1,16 @@
-def bottle_song():
-	bottles = 99
+def bottle_song(num):
+	bottles = num
 		
 	while bottles > 0:
-		print(f"{bottles}{is_plural(bottles)} of beer on the wall {bottles}{is_plural(bottles)} of beer. Take one down and pass it around, {bottles-1}{is_plural(bottles-1)} of beer on the wall.")
+		print(f"{bottles} {is_plural(bottles)} of beer on the wall {bottles} {is_plural(bottles)} of beer. Take one down and pass it around, {bottles-1} {is_plural(bottles-1)} of beer on the wall.")
 		bottles -= 1
 	if bottles == 0:
-		print(f"No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall ")
+		print(f"No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall ")
 
 #Helper function to determine if "Bottles" in the string is plural
 
 def is_plural(bottles):
-	if bottles >1:
+	if bottles >1 or bottles == 0:
 		return "bottles"
 	return "bottle"
 		
